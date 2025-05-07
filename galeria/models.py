3306ffa813d17cd44f2df36ed38d320312101eef
Nicolly <nicollyengenheira@gmail.com>
2025-05-07 14:23:36 -0300

    Refactor

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime
+
+from django.contrib.auth.models import User
 
 
 class Fotografia(models.Model):
@@ -13,9 +16,18 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
-    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA,
+                                 default='')
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="user"
+    )
 
     def __str__(self):
         return self.nome
